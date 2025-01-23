ab2567b506faa9ef0a8a22c3a7a47d57d4b2fd52
Ian <138981016+ianmos13@users.noreply.github.com>
2025-01-23 15:09:31 +0100

    Update server.ts

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,6 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('LN1eqCoIjdECDKivyn356g==','r3dRuMR2vN90UwNBqcRUVA==','dareb9QJT1g8S0aX8QrQxA==','gRclr2A4MqGuXe/SeqVnYA=='),
+    keys: env.array('APP_KEYS'),
   },
 });
