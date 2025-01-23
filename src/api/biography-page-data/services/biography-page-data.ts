b8177d2bea8fdbfe84b6426ed1a6f3c1fefb596c
Max Mendelev <mmendelev@navirit.by>
2025-01-23 16:11:17 +0300

    Add 2 iteration

---
+++ src/api/biography-page-data/services/biography-page-data.ts
@@ -0,0 +1,7 @@
+/**
+ * biography-page-data service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::biography-page-data.biography-page-data');
