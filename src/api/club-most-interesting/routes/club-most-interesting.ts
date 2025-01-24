fb0265b0afedded859c7893dbec8da41df87e9fe
Max Mendelev <mmendelev@navirit.by>
2025-01-24 18:39:29 +0300

    Add club most interesting

---
+++ src/api/club-most-interesting/routes/club-most-interesting.ts
@@ -0,0 +1,7 @@
+/**
+ * club-most-interesting router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::club-most-interesting.club-most-interesting');
